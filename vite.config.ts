d697c7240e66efd513d245b0100c63db573174a6
smitike <selammitike8@gmail.com>
2026-05-24 18:51:41 -0700

    github page deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+// export default defineConfig({
+//   plugins: [react(), tailwindcss(), tsconfigPaths()],
+// });
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  base: "my-linktree",
+  plugins: [react()],
 });
