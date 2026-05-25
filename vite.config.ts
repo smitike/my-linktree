72c6b3f6dfd11b45595b73ccab442cee5d2b7820
smitike <selammitike8@gmail.com>
2026-05-24 19:00:31 -0700

    back to netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
-// export default defineConfig({
-//   plugins: [react(), tailwindcss(), tsconfigPaths()],
-// });
 export default defineConfig({
-  base: "/my-linktree/",
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
 });
+// export default defineConfig({
+//   base: "/my-linktree/",
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
