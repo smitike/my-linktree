0f4e3a4f82c3a7ff5b10787162cba183b8335aac
smitike <selammitike8@gmail.com>
2026-05-24 18:58:07 -0700

    Vite path alias for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
+import path from "path";
 // export default defineConfig({
 //   plugins: [react(), tailwindcss(), tsconfigPaths()],
 // });
 export default defineConfig({
-  base: "my-linktree",
+  base: "/my-linktree/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
